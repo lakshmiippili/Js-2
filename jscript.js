e7af4199aa11691a09dc6faca2c05f2957e9f032
lakshmiippili <116436471+lakshmiippili@users.noreply.github.com>
2022-11-16 21:42:31 +0530

    fourth commit done

--- jscript.js
+++ jscript.js
@@ -1,46 +1,21 @@
-class Student{
-    //static count=0
-    constructor(name,age,phone,marks){
-        this.name=name
-        this.age=age
-        //this.phone=phone
-        this.marks=marks
-        //Student.count++
-    }
-    // isEligible(){
-    //     if(this.marks>40){
-    //         console.log('eligilble for college')
-    //     }else{
-    //         console.log('not eligible for college')
-    //     }
-    // }
-    // static studentsCreated(){
-    //     console.log(this.count)
-    // }
-    // printDetails(){
-    //     console.log(`${this.name}:${this.age}:${this.phone}:${this.marks}`)
-    // }
-    ageForPlacement(minAge){
-        return (minMarks)=>{
-            if(this.marks>minMarks && this.age>minAge){
-                console.log(`${this.name} is eligible for placements`)
-            }else{
-                console.log(`${this.name} is not eligible for placements`)
-            }
-        }
-    }
+"use strict"
+let getA = (a)=> a
+console.log(getA(5))
+
+let square= (a)=>{ return a*a}
+console.log(square(5))
+
+var a=4
+let sq=()=>{ return a*a}
+console.log(sq())
+
+let mul = (a,b)=>{ return a*b}
+console.log(mul(5,2))
+
+var x= function(){
+    this.val = 1
+    setTimeout(()=>{
+        this.val++
+        console.log(this.val)},1)
 }
-var student1= new Student('lakshmi',26,123,44)
-//student1.printDetails()
-var student2=new Student('mani',31,234,34)
-//Student.studentsCreated()
-var student3=new Student('kumar',32,345,50)
-var student4=new Student('bhavani',27,456,72)
-var student5= new Student('venkat',29,567,65)
-// student1.isEligible()
-// student2.isEligible()
-// Student.studentsCreated()
-student1.ageForPlacement(20)(40)
-student2.ageForPlacement(20)(40)
-student3.ageForPlacement(20)(40)
-student4.ageForPlacement(20)(40)
+var y= new x()
